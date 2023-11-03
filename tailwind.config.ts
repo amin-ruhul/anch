d971e1c347c2bca5fb43afb4fe58fd7acfcb2939
Ruhul <ruhulinfo50@gmail.com>
2023-11-03 09:49:50 +0600

    Add color to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+
+      colors: {
+        primary: {
+          default: "#377DFF",
+        },
+        secondary: {
+          500: "#EBEDF0",
+          900: "#D2D4D6",
+          default: "#EBEDF0",
+        },
+        gray: {
+          400: "#EDEFF1",
+          500: "#F0F5FA",
+          600: "#F3F3F3",
+          700: "#B0B7C3",
+          800: "#8A94A6",
+        },
+
+        "error-color": "#FF5630",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
